7dac8dc7f4870051c72067fe48dd245e366e77e2
kimbom <kingbom_300432@hotmail.com>
2021-08-11 23:05:09 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,6 @@ const pdfService = require('../services/pdf-service');
 const router = expres.Router();
 
 router.get('/invoice', (req, res) => {
-    // const stream = res.writeHead(200, {
-    //     'Content-Type': 'application/pdf',
-    //     'Content-Disposition': 'attachment;filename=invoice.pdf'
-    // });
     pdfService.generatePdf();
 });
 
